9f97eb901425c776c04a1c508f94b1b43af00b71
Zain Razzaq <zainrazzaq2003@gmail.com>
2025-10-18 09:50:31 +0500

    added env in client

--- client/src/api/axios-instance.ts
+++ client/src/api/axios-instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as LocalAuthManager from "@/lib/LocalAuthManager"
 import { toast } from "sonner";
 
-const API_BASE_URL = "http://127.0.0.1:8000/";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
